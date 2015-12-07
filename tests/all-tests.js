8672bfe9d9ac434f66ef4a9b76d8e6cfca24f0bc
Tony Atkins <duhrer@gmail.com>
2015-12-07 10:40:04 +0100

    GPII-1528:  Suppress warnings about max event listeners (related to Nightmare bug).

--- tests/all-tests.js
+++ tests/all-tests.js
@@ -1,3 +1,18 @@
+/*
+
+    Nightmare does not currently clean up all of its listeners before it's destroyed, which results in warnings like the
+    following when running more than 10 tests:
+
+    ```
+    (node) warning: possible EventEmitter memory leak detected. 11 uncaughtException listeners added. Use emitter.setMaxListeners() to increase limit.
+    ```
+
+    For now, we increase the number of listeners to supress the warning.
+
+    TODO:  Remove the workaround once this issue is resolved: https://github.com/segmentio/nightmare/issues/282
+ */
+process.setMaxListeners(25);
+
 require("./js/browser-check-tests.js");
 require("./js/browser-click-tests.js");
 require("./js/browser-exists-tests.js");
